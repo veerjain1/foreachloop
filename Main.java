ae3cf5962669ee17f715dc4860b10d459bf5d230
Veer Jain <111719388+veerjain1@users.noreply.github.com>
2022-11-12 19:57:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+  int[] keekee = {4,31,22,5};
+
+    for(int jink: keekee){
+
+      jink++;
+      System.out.println(jink);
+    
+  }
+
+}
+}
